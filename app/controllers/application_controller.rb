e16938701e95695891fd806bd2695ffc88cd9050
Mustafa Ahmed <mustafa.de1.ahmed@gmail.com>
2017-10-10 23:31:11 +0000

    First commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+   http_basic_authenticate_with name: "dhh", password: "secret", except: :index
   protect_from_forgery with: :exception
 
   def hello
